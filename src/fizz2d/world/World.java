d9560a1861e57a7bc20fecf42856aa1838105979
Scott Davey <me@sdavey.uk>
2017-04-04 19:18:35 +0100

    Committing intentions more than anything

---
+++ src/fizz2d/world/World.java
@@ -0,0 +1,10 @@
+package fizz2d.world;
+
+/**
+ * Created by Scott Davey on 03/04/2017.
+ */
+public class World {
+    private WorldConfiguration worldConfiguration;
+
+
+}
---
+++ src/fizz2d/world/WorldConfiguration.java
@@ -0,0 +1,8 @@
+package fizz2d.world;
+
+/**
+ * Created by Scott Davey on 03/04/2017.
+ */
+public class WorldConfiguration {
+
+}
